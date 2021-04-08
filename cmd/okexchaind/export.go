38e8086656eea6466fa0dc796360f64eabf3b7de
meng <805442788@qq.com>
2021-04-08 19:01:44 +0800

    pruning

---
+++ cmd/okexchaind/export.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/cosmos/cosmos-sdk/store/rootmulti"
+	"github.com/okex/okexchain/app"
+	"log"
+	"path/filepath"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/spf13/cobra"
+)
+
+func exportAppCmd(ctx *server.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "exportApp",
+		Short: "export current snapshot to new db",
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Println("--------- export start ---------")
+			export(ctx)
+			log.Println("--------- export success ---------")
+		},
+	}
+	return cmd
+}
+
+func export(ctx *server.Context) {
+	fromApp := createApp(ctx, "data")
+	toApp := createApp(ctx, "export")
+
+	fromRs, _ := fromApp.BaseApp.GetCms().(*rootmulti.Store)
+	toRs := toApp.BaseApp.GetCms().(*rootmulti.Store)
+
+	err := fromRs.Export(toRs, fromRs.LastCommitID().Version)
+	if err != nil {
+		panicError(err)
+	}
+}
+
+func createApp(ctx *server.Context, dataPath string) *app.OKExChainApp {
+	rootDir := ctx.Config.RootDir
+	dataDir := filepath.Join(rootDir, dataPath)
+	db, err := openDB(applicationDB, dataDir)
+	panicError(err)
+	exapp := newApp(ctx.Logger, db, nil)
+	return exapp.(*app.OKExChainApp)
+}
